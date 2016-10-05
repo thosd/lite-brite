b1a710163dfd9617bb6d9fef185ee3e1bf06ece1
thosd <thos.deaton@gmail.com>
2016-10-05 18:39:37 -0400

    fixed previous commit

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var express = require ('express');
 var server = express ();
 
 
+server.use(express.static(__dirname + '/public'));
+
 server.get('/', function(request, response){
   response.sendFile('public/html/index.html', { root: __dirname });
 });
