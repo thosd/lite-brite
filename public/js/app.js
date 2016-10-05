f01345f21a4dcc5862018c3071bf0885e08dc4ed
thosd <thos.deaton@gmail.com>
2016-10-05 19:48:42 -0400

    created grid initialization

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,20 @@
-alert('I work');
+
+(function() {
+
+    var numberOfRows = 40; //number of rows in the grid
+    var numberOfCols = 40; //number of columns in the grid
+    var canvas = $('#canvas'); //my placement area - think of paper in drawing
+
+    //let's make some rows and put them in the body
+    for(var rowCount = 0; rowCount < numberOfRows; rowCount += 1){
+      var row = $('<tr></tr>');
+      for(var colCount = 0; colCount < numberOfCols; colCount += 1){
+        var column = $('<td><td>');
+        column.addClass('cell');
+        row.append(column);
+      }
+      canvas.append(row);
+    }
+
+
+}());
