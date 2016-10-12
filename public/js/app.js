59ae1747b9e6eb728eaa668a061ee0ca994bdce6
thosd <thos.deaton@gmail.com>
2016-10-12 18:17:06 -0400

    Added prompts to user grid size input

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,59 @@
 
 (function() {
 
-    //var numberOfRows = 15; //number of rows in the grid
-    //var numberOfCols = 15;//number of columns in the grid
     var canvas = $('#canvas'); //my placement area - think of paper in drawing
+    var updateGridButton = $('#update-grid-button');
+    var numberOfColsInput = $('#number-of-cols');
+    var numberOfRowsInput = $('#number-of-rows');
 
     makeGrid(15, 15);
-    clearGrid();
-    makeGrid(30, 30);
-    var cells = $('.cell');
     $('.cell').on('click', changeColor);
+    updateGridButton.on('click', updateGridSize);
+    //clearGrid();
+    //makeGrid(30, 15);
+
+    function valBetween(v, min, max) {
+        return (Math.min(max, Math.max(min, v)));
+    }
+
+
+
+
+
+    function updateGridSize(){
+      clearGrid();//remove the current grid
+      //grab the number of columns from the input for the new grid
+      var newColNumber = parseInt(numberOfColsInput.val());
+      if (!numberOfColsInput.val()) {
+        alert("Please enter values!");
+        clearGrid();
+        return;
+      }
+      if (newColNumber > 100) {
+        alert("Max grid size exceeded.  Please choose a value between 1 & 100");
+        clearGrid();
+        return;
+      }
+      newColNumber = valBetween(newColNumber, 1, 100);
+      //grab the number of rows from the input for the new grid
+      var newRowNumber = parseInt(numberOfRowsInput.val());
+      if (!numberOfRowsInput.val()) {
+        alert("Please enter values!");
+        clearGrid();
+        return;
+
+      }
+      if (newRowNumber > 100) {
+        alert("Max grid size exceeded.  Please choose a value between 1 & 100");
+        clearGrid();
+        return;
+      }
+      newRowNumber = valBetween(newRowNumber, 1, 100);
+      //make the new grid based on the new rows and columns
+
+      makeGrid(newRowNumber, newColNumber);
+      $('.cell').on('click', changeColor);
+    }
 
     function clearGrid(){
       canvas.empty();
@@ -20,11 +64,10 @@
     }
     function makeGrid(numberOfRows, numberOfCols){
       //let's make some rows and put them in the body
-      for(var rowCount = 0; rowCount < numberOfRows; rowCount += 1){
+      for(var rowCount = 0; rowCount < numberOfRows; rowCount += 1) {
         var row = $('<tr></tr>');
-        for(var colCount = 0; colCount < numberOfCols; colCount += 1){
-          console.log(colCount);
-          var column = $('<td><td>');
+        for(var colCount = 0; colCount < numberOfCols; colCount += 1) {
+          var column = $('<td></td>');
           column.addClass('cell');
           row.append(column);
         }
